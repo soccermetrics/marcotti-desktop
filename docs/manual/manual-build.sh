2347914224bdd7d23622538c65937218c909a4b3
Howard Hamilton <hhamilton@soccermetrics.net>
2011-08-02 16:31:34 -0400

    Removed files from docs/ directory

--- docs/manual/manual-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-elyxer --destdirectory images/ --title "FMRD User Manual" fmrd-manual.lyx fmrd-manual.html
-elyxer --tocfor fmrd-manual.html --css docs/toc.css --dest-directory images/ --target contents fmrd-manual.lyx fmrd-manual-toc.html
