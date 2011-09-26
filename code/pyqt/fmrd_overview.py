49d1cb8c730f1e32a3732a05d3a35fa1ee0dbd44
Howard Hamilton <hhamilton@soccermetrics.net>
2011-09-26 19:46:06 -0400

    Initialized confed and timezone combobox indices to -1

--- code/pyqt/fmrd_overview.py
+++ code/pyqt/fmrd_overview.py
@@ -640,6 +640,8 @@ class VenueEntryDlg(QDialog, ui_venueentry.Ui_VenueEntryDlg):
         self.timezoneModel.setFilter(QString("confed_id = %1").arg(id))
         self.countryModel.select()
         self.timezoneModel.select()
+        self.venueTimezoneSelect.setCurrentIndex(-1)
+        self.venueConfedSelect.setCurrentIndex(-1)        
     
     def openVenueHistory(self, venue_id):
         """Opens Venue History subdialog for a specific match from Match dialog.
