0ea2fa9ea9bb17c9f979618958c46fa36ac9cf79
Howard Hamilton <hhamilton@soccermetrics.net>
2012-01-01 02:01:31 -0500

    Widen layout widget box on Sqlite db dialog (#97)

--- code/pyqt/FmrdMain/ui_fmrddbfile.py
+++ code/pyqt/FmrdMain/ui_fmrddbfile.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'gui/fmrd_dbfile.ui'
 #
-# Created: Thu Dec 29 23:31:04 2011
+# Created: Sat Dec 31 21:55:18 2011
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -15,14 +15,14 @@ class Ui_DBFileLoadDlg(object):
         DBFileLoadDlg.resize(240, 120)
         DBFileLoadDlg.setMinimumSize(QtCore.QSize(240, 120))
         DBFileLoadDlg.setMaximumSize(QtCore.QSize(240, 120))
-        self.widget = QtGui.QWidget(DBFileLoadDlg)
-        self.widget.setGeometry(QtCore.QRect(30, -3, 186, 121))
-        self.widget.setObjectName("widget")
-        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
+        self.layoutWidget = QtGui.QWidget(DBFileLoadDlg)
+        self.layoutWidget.setGeometry(QtCore.QRect(25, -3, 191, 121))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setObjectName("verticalLayout")
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.userButton = QtGui.QRadioButton(self.widget)
+        self.userButton = QtGui.QRadioButton(self.layoutWidget)
         self.userButton.setMinimumSize(QtCore.QSize(60, 30))
         self.userButton.setMaximumSize(QtCore.QSize(60, 30))
         self.userButton.setChecked(True)
@@ -30,13 +30,13 @@ class Ui_DBFileLoadDlg(object):
         self.horizontalLayout.addWidget(self.userButton)
         spacerItem = QtGui.QSpacerItem(13, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
-        self.adminButton = QtGui.QRadioButton(self.widget)
+        self.adminButton = QtGui.QRadioButton(self.layoutWidget)
         self.adminButton.setMinimumSize(QtCore.QSize(75, 30))
         self.adminButton.setMaximumSize(QtCore.QSize(75, 30))
         self.adminButton.setObjectName("adminButton")
         self.horizontalLayout.addWidget(self.adminButton)
         self.verticalLayout.addLayout(self.horizontalLayout)
-        self.openButton = QtGui.QPushButton(self.widget)
+        self.openButton = QtGui.QPushButton(self.layoutWidget)
         self.openButton.setMinimumSize(QtCore.QSize(180, 30))
         self.openButton.setMaximumSize(QtCore.QSize(180, 30))
         self.openButton.setObjectName("openButton")
@@ -45,7 +45,7 @@ class Ui_DBFileLoadDlg(object):
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem1)
-        self.cancelButton = QtGui.QPushButton(self.widget)
+        self.cancelButton = QtGui.QPushButton(self.layoutWidget)
         self.cancelButton.setMinimumSize(QtCore.QSize(90, 30))
         self.cancelButton.setMaximumSize(QtCore.QSize(90, 30))
         self.cancelButton.setObjectName("cancelButton")
